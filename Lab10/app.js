df8dd807b3892d3e14b42fb45a118874eaed9b43
yousefsalem <yousef_salem97@yahoo.com>
2022-07-05 22:44:45 -0500

    modified lab10

--- Lab10/app.js
+++ Lab10/app.js
@@ -1,4 +1,10 @@
-let MyArray = require("./myArr");
-let myArr = new MyArray([1,2,3,4,5,6,7,8]);
-console.log(myArr.odd());
-console.log(myArr.even()); 
+Array.prototype.even = function(){
+    return this.filter(num=>num%2===0);
+}
+Array.prototype.odd = function(){
+    return this.filter(num=>num%2!==0);
+}
+
+let arr = [1,2,3,4,5,6,7,8]
+console.log(arr.odd());
+console.log(arr.even()); 
